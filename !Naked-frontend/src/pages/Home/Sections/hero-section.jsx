b0a7ae357f06a33112ad760e335d33a3bff3de75
Agent-75 <abbass.babass10@gmail.com>
2024-02-17 09:10:02 +0200

    Fix responsitivity of HeroSection in Home page

--- !Naked-frontend/src/pages/Home/Sections/hero-section.jsx
+++ !Naked-frontend/src/pages/Home/Sections/hero-section.jsx
@@ -4,45 +4,49 @@ import HeroSectionGirl from "../../../assets/HeroSectionGirl.png";
 const Container = styled.section`
 background:white;
 width:100%;
-height:83vh;
 
-@media screen and (max-width:800px){
-    background : white;
-    background-position:top right;
+@media screen and (max-width:1024px){
     height:auto;
-    max-height:none; 
-}
-
-@media screen and (min-width:800px) and (min-height:1000px){
-    max-height: 45vh;
 }
 `
 
 const Content =styled.div`
-gap:6rem;
+gap:4rem;
 width:100%;
 height:100%;
 display:flex;
 align-items:center;
 justify-content:space-between;
-border:1px solid black;
-@media screen and (max-width:800px){
-    flex-direction:column;
+@media screen and (max-width:1024px){
+    flex-direction:column-reverse;
+    align-items:flex-start;
 }
+
+@media screen and (max-width:800px){
+    gap:2rem;
+}
+
 `
 const ImageSection = styled.div`
-// flex:1;
-width:50%;
-
+width:48.6%;
 height:100%;
 display:flex;
-align-items:center;
-justify-content:flex-end;
-background:blue;
 position:relative;
-@media screen and (max-width:800px){
-    display:block;
+align-items:center;
+padding: 1rem  0;
+justify-content:flex-end;
+
+@media screen and (max-width:1200px){
+    width:43%;
 }
+@media screen and (max-width:1024px){
+    width:calc(100% - 2rem);
+    padding: 2rem 0;
+}
+@media screen and (max-width:800px){
+    padding: 1rem 0;
+}
+
 `
 const ImageSectionBackground = styled.div`
 top:0;
@@ -53,48 +57,76 @@ position:absolute;
 background:var(--main-color);
 `
 const ImageContainer = styled.div`
-width:55%;
+width:68%;
 position:relative;
 aspect-ratio:1/1.34;
-box-shadow: 16px 10px 47px -16px #00C2FF;
+
+@media screen and (max-width:1400px){
+    width:60%;
+}
+
+@media screen and (max-width:1200px){
+    width:72%;
+}
+
+@media screen and (max-width:1000px){
+    width:86%;
+}
+
+@media screen and (max-width:800px){
+    width:90%;
+}
 `
 const Image = styled.img`
+box-shadow: 16px 10px 47px -16px #00C2FF;
+
 width:100%;
 height:100%;
 object-fit:cover;
 `
 const TextSection =styled.div`
-// flex:.8;
-// width:80%;
 width:40%;
 display:flex;
-padding-right:2rem;
+padding:0 2rem 0 0;
 justify-content:flex-end;
-
+@media screen and (max-width:1200px){
+    width:48%;
+}
+@media screen and (max-width:1024px){
+    width:60%;
+    padding:2rem 0 0 2rem;
+}
+@media screen and (max-width:800px){
+    width:100%;
+    padding:1rem 1rem 0 1rem;
+}
 `
 const TextContainer = styled.div`
 gap:4rem;
 display:flex;
 flex-direction:column;
+@media screen and (max-width:800px){
+    gap:2rem;
+}
 `
 const TextWrapper = styled.div`
 gap:2rem;
 display:flex;
 flex-direction:column;
 @media screen and (max-width:800px){
-    text-align:center;
-    margin-top:1rem;
-    gap:30px;
+    gap:1.5rem;
 }
 `
 const MainText =styled.h1`
 font-weight:bold;
-// min-width:500px;
-font-size:var(--heading-1);
-line-height:105.6%;
+font-size: var(--big-1);
+line-height:105px;
 letter-spacing:-0.02em;
 position:relative;
 padding-bottom:2rem;
+@media screen and (max-width:800px){
+    padding-bottom:1.5rem;
+}
 &:before{
     content : "";
     left: 0;
@@ -102,29 +134,79 @@ padding-bottom:2rem;
     height: 1px;
     width: 35%;  
     position:absolute;
-    border-bottom:5px solid #00C2FF;
+    border-bottom:8px solid #00C2FF;
+    @media screen and (max-width:1400px){
+        border-bottom:5px solid #00C2FF;
+    }
+}
+
+@media screen and (max-width:1820px){
+    font-size:var(--big-2);
+    line-height:83px;
+}
+
+@media screen and (max-width:1470px){
+    font-size:var(--heading-1);
+    line-height:66px;
+}
+
+@media screen and (max-width:600px){
+    line-height:35.2px;
+    font-size:var(--heading-1-mobile);
 }
 `
 const SecondaryText =styled.p`
-font-size:var(--heading-6);
-font-weight:600;
 color:grey;
+font-weight:600;
+line-height:40px;
+font-size:var(--heading-4);
+
+@media screen and (max-width:1820px){
+    font-size:var(--heading-5);
+    line-height:32.5px;
+}
+
+@media screen and (max-width:1470px){
+    font-size:var(--heading-6);
+    line-height:26px;
+}
+
+@media screen and (max-width:600px){
+    line-height:20px;
+    font-size:var(--body);
+}
 `
 const ShopNowButton  = styled.button`
-width:40%;
-height:40px;
+width:50%;
+height:50px;
 color:white;
 outline:none;
 border:none;
-font-size:1rem;
+cursor:pointer;
 max-width:312px;
 font-weight:600;
 border-radius:3px;
+transition:background .3s;
+font-size:var(--heading-5);
 background:var(--main-color);
 box-shadow:10px 9px 12px -3px rgba(0,0,0,0.4) ;
+@media screen and (max-width:1470px){
+    font-size:var(--body);
+    height:40px;
+}
+
 @media screen and (max-width:800px){
-    font-size:.8rem;
-    width:100%;
+    width:45%;
+    min-width:164px;
+    height:40px;
+    max-width:none;
+}
+@media screen and (max-width:600px){
+    height:35px;
+}
+
+&:hover{
+    background:#009BCC;
 }
 `
 
